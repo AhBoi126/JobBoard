054ef6320f5284c72c8c246a8dfc3645e490fd2d
Terran56 <myothiha7584@gmail.com>
2025-01-22 10:45:11 +0800

    more seed

---
+++ JobBoard/Configurations/Entities/JobSeekerSeed.cs
@@ -0,0 +1,61 @@
+﻿using FullStackProject.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace FullStackProject.Configurations.Entities
+{
+    public class JobSeekerSeed : IEntityTypeConfiguration<JobSeeker>
+    {
+        public void Configure(EntityTypeBuilder<JobSeeker> builder)
+        {
+            builder.HasData(
+                new JobSeeker
+                {
+                    Id = 1,
+                    FirstName = "Wei Ming",
+                    LastName = "Tan",
+                    Email = "weiming.tan@example.com",
+                    Contact = "9123-4567",
+                    Skills = "C#, ASP.NET, SQL",
+                    ProfilePicturePath = "images/wei_ming_tan.jpg",
+                    Location = "Tiong Bahru, Singapore",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new JobSeeker
+                {
+                    Id = 2,
+                    FirstName = "Xiu Ying",
+                    LastName = "Lim",
+                    Email = "xiuying.lim@example.com",
+                    Contact = "8123-7654",
+                    Skills = "JavaScript, React, Node.js",
+                    ProfilePicturePath = "images/xiuying_lim.jpg",
+                    Location = "Queenstown, Singapore",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new JobSeeker
+                {
+                    Id = 3,
+                    FirstName = "Ah Huat",
+                    LastName = "Chong",
+                    Email = "ahhuat.chong@example.com",
+                    Contact = "9234-5678",
+                    Skills = "Python, Data Science, Machine Learning",
+                    ProfilePicturePath = "images/ahhuat_chong.jpg",
+                    Location = "Changi, Singapore",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+            );
+        }
+    }
+}
---
+++ JobBoard/Configurations/Entities/ResumeSeed.cs
@@ -0,0 +1,46 @@
+﻿using FullStackProject.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace FullStackProject.Configurations.Entities
+{
+    public class ResumeSeed : IEntityTypeConfiguration<Resume>
+    {
+        public void Configure(EntityTypeBuilder<Resume> builder)
+        {
+            builder.HasData(
+                new Resume
+                {
+                    Id = 1,
+                    JobSeekerID = 1,  // References JobSeeker with Id 1
+                    ResumeFilePath = "resumes/wei_ming_tan_resume.pdf",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Resume
+                {
+                    Id = 2,
+                    JobSeekerID = 2,  // References JobSeeker with Id 2
+                    ResumeFilePath = "resumes/xiuying_lim_resume.pdf",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Resume
+                {
+                    Id = 3,
+                    JobSeekerID = 3,  // References JobSeeker with Id 3
+                    ResumeFilePath = "resumes/ahhuat_chong_resume.pdf",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+            );
+        }
+    }
+}
